29f71975ef67ef26b4bb444a5004b857686bbfb1
Minasov Inc <minasoverik@gmail.com>
2021-11-04 19:46:41 +0300

    commit

--- lib/models/local_notify_manager.dart
+++ lib/models/local_notify_manager.dart
@@ -1,20 +1,20 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class LocalNotifyManager {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-final AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('app_icon');
-final IOSInitializationSettings initializationSettingsIOS =
-    IOSInitializationSettings(
-        // ignore: avoid_types_as_parameter_names
-        onDidReceiveLocalNotification: (int id, String title, String body, String) async {});
-final MacOSInitializationSettings initializationSettingsMacOS =
-    MacOSInitializationSettings();
-final InitializationSettings initializationSettings = InitializationSettings(
-    android: initializationSettingsAndroid,
-    iOS: initializationSettingsIOS,
-    macOS: initializationSettingsMacOS);
-await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-    onSelectNotification: selectNotification);
-}
+// class LocalNotifyManager {
+//   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
+// final AndroidInitializationSettings initializationSettingsAndroid =
+//     AndroidInitializationSettings('app_icon');
+// final IOSInitializationSettings initializationSettingsIOS =
+//     IOSInitializationSettings(
+//         // ignore: avoid_types_as_parameter_names
+//         onDidReceiveLocalNotification: (int id, String title, String body, String) async {});
+// final MacOSInitializationSettings initializationSettingsMacOS =
+//     MacOSInitializationSettings();
+// final InitializationSettings initializationSettings = InitializationSettings(
+//     android: initializationSettingsAndroid,
+//     iOS: initializationSettingsIOS,
+//     macOS: initializationSettingsMacOS);
+// await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+//     onSelectNotification: selectNotification);
+// }
